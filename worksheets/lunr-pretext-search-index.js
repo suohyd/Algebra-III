dc3279bc20647f31960afc854095412c3c75ea16
suohyd <86884865+suohyd@users.noreply.github.com>
2025-08-29 14:39:35 +0530

    Latest build deployed.

--- worksheets/lunr-pretext-search-index.js
+++ worksheets/lunr-pretext-search-index.js
@@ -188,6 +188,60 @@ var ptx_lunr_docs = [
   "number": "8",
   "title": "",
   "body": "  Let be a field extension of degree . Let .     Show that the following map is -linear.     Suppose that be a matrix of the linear map . Show that is a root of the characteristic polynomial of . This gives a procedure to determine a polynomial of degree satisfying an element in an extension of degree over     Using the procedure outlined in the above two sub-questions, find a monic polynomial of degree satisfied by (resp. ).    Show that is isomorphic to a subfield of . So the ring contains an isomorphic copy of every field extension of of degree at most .    Let and let . Let .     Show that the matrix of with respect to the basis is the following.     Prove directly that the following is an isomorphism of with a subfield of .          "
+},
+{
+  "id": "activity-tutorial-4",
+  "level": "1",
+  "url": "activity-tutorial-4.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Tutorial-4",
+  "body": " Tutorial-4   Following exercises are taken from Sections 13.3 and 13.4 of the book Abstract Algebra by Dummit and Foote.      Show that it is impossible to construct a regular -gon with straightedge and compass.      Use the fact that satisfies the equation to construct a regular pentagon with straightedge and compass.      Determine the splittting field and degree over of the following polynomials.       Let be a finite field extension. Show that is a splitting field over if and only if every irreducible polynomial in that has a root in splits completely in .      Let be finite extensions of a field contained in a field . Furthermore, assume that are splitting fields over . Show that and are splitting fields over .     "
+},
+{
+  "id": "activity-tutorial-4-3-1",
+  "level": "2",
+  "url": "activity-tutorial-4.html#activity-tutorial-4-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Show that it is impossible to construct a regular -gon with straightedge and compass.   "
+},
+{
+  "id": "activity-tutorial-4-3-2",
+  "level": "2",
+  "url": "activity-tutorial-4.html#activity-tutorial-4-3-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Use the fact that satisfies the equation to construct a regular pentagon with straightedge and compass.   "
+},
+{
+  "id": "activity-tutorial-4-3-3",
+  "level": "2",
+  "url": "activity-tutorial-4.html#activity-tutorial-4-3-3",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Determine the splittting field and degree over of the following polynomials.    "
+},
+{
+  "id": "activity-tutorial-4-3-4",
+  "level": "2",
+  "url": "activity-tutorial-4.html#activity-tutorial-4-3-4",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Let be a finite field extension. Show that is a splitting field over if and only if every irreducible polynomial in that has a root in splits completely in .   "
+},
+{
+  "id": "activity-tutorial-4-3-5",
+  "level": "2",
+  "url": "activity-tutorial-4.html#activity-tutorial-4-3-5",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Let be finite extensions of a field contained in a field . Furthermore, assume that are splitting fields over . Show that and are splitting fields over .   "
 }
 ]
 

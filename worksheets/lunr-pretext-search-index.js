6e2d3d094176c1a418f9fe0ef125d0e8ecc27804
suohyd <86884865+suohyd@users.noreply.github.com>
2025-08-29 14:19:24 +0530

    Latest build deployed.

--- worksheets/lunr-pretext-search-index.js
+++ worksheets/lunr-pretext-search-index.js
@@ -107,6 +107,87 @@ var ptx_lunr_docs = [
   "number": "5",
   "title": "",
   "body": "  Show that and are irreducible over .   "
+},
+{
+  "id": "activity-tutorial-3",
+  "level": "1",
+  "url": "activity-tutorial-3.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Tutorial-3",
+  "body": " Tutorial-3   Following exercises are taken from Section 13.2 of the book Abstract Algebra by Dummit and Foote.      Show that the field is the compositum of fields .      Show that . Find the minimal polynomial of over .      Let . Let . Show the following. When the field extension is called a biquadratic extension .      Let . Suppose that and that . Show that for some if and only if . Use this to determine when the field extension has degree .      Suppose that is such that for . Show that the real cube root of does not belong to .      If is odd then, .      Let be a field and let be the field of rational functions in one variable over . Let , where , , and . Assume that . Compute using the following steps.     Show that is irreducible over and that is a root of .    Show that .    Show that .         Let be a field extension of degree . Let .     Show that the following map is -linear.     Suppose that be a matrix of the linear map . Show that is a root of the characteristic polynomial of . This gives a procedure to determine a polynomial of degree satisfying an element in an extension of degree over     Using the procedure outlined in the above two sub-questions, find a monic polynomial of degree satisfied by (resp. ).    Show that is isomorphic to a subfield of . So the ring contains an isomorphic copy of every field extension of of degree at most .    Let and let . Let .     Show that the matrix of with respect to the basis is the following.     Prove directly that the following is an isomorphism of with a subfield of .            "
+},
+{
+  "id": "activity-tutorial-3-3-1",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Show that the field is the compositum of fields .   "
+},
+{
+  "id": "activity-tutorial-3-3-2",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Show that . Find the minimal polynomial of over .   "
+},
+{
+  "id": "activity-tutorial-3-3-3",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-3",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Let . Let . Show the following. When the field extension is called a biquadratic extension .   "
+},
+{
+  "id": "activity-tutorial-3-3-4",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-4",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Let . Suppose that and that . Show that for some if and only if . Use this to determine when the field extension has degree .   "
+},
+{
+  "id": "activity-tutorial-3-3-5",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-5",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Suppose that is such that for . Show that the real cube root of does not belong to .   "
+},
+{
+  "id": "activity-tutorial-3-3-6",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-6",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  If is odd then, .   "
+},
+{
+  "id": "activity-tutorial-3-3-7",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-7",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Let be a field and let be the field of rational functions in one variable over . Let , where , , and . Assume that . Compute using the following steps.     Show that is irreducible over and that is a root of .    Show that .    Show that .      "
+},
+{
+  "id": "activity-tutorial-3-3-8",
+  "level": "2",
+  "url": "activity-tutorial-3.html#activity-tutorial-3-3-8",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Let be a field extension of degree . Let .     Show that the following map is -linear.     Suppose that be a matrix of the linear map . Show that is a root of the characteristic polynomial of . This gives a procedure to determine a polynomial of degree satisfying an element in an extension of degree over     Using the procedure outlined in the above two sub-questions, find a monic polynomial of degree satisfied by (resp. ).    Show that is isomorphic to a subfield of . So the ring contains an isomorphic copy of every field extension of of degree at most .    Let and let . Let .     Show that the matrix of with respect to the basis is the following.     Prove directly that the following is an isomorphism of with a subfield of .          "
 }
 ]
 
